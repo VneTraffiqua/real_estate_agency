2217bbdf2667156a36e0c48bbfe51de6feae036c
VneTraffiqua <safe72again@gmail.com>
2022-10-18 00:00:43 -0700

    Create 0012_auto_20221014_1647.py

---
+++ property/migrations/0012_auto_20221014_1647.py
@@ -0,0 +1,15 @@
+import phonenumbers
+from django.db import migrations
+
+
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('property', '0011_flat_owner_pure_phone'),
+    ]
+
+    operations = [
+
+    ]
