01e1d3debe7a2519e44c2ecb47fe802481f3448e
VneTraffiqua <safe72again@gmail.com>
2022-10-21 02:19:52 -0700

    Add owner_pure_phone

--- property/models.py
+++ property/models.py
@@ -7,7 +7,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owner_pure_phone = PhoneNumberField(
-        'Нормализованный номер владельца', blank=True, max_length=20
+        'Нормализованный номер владельца', blank=True, null=True, max_length=20
     )
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     created_at = models.DateTimeField(
