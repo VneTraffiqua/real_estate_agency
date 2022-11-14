cfc4c2a3cc8eae7d696dda13eb128f007c4be1e7
VneTraffiqua <safe72again@gmail.com>
2022-11-14 06:18:50 -0800

    Update admin

--- property/admin.py
+++ property/admin.py
@@ -3,28 +3,24 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class PropertyownerInline(admin.TabularInline):
+    model = Flat.owners_flats.through
+    raw_id_fields = ('owner', 'flat')
+
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'town_district', 'address')
-    readonly_fields = ['created_at']
+    readonly_fields = ('created_at',)
+    list_display = ('address','price','new_building','construction_year', 'town')
+    list_editable = ('new_building',)
+    list_filter = ('new_building', 'rooms_number', 'has_balcony', 'floor')
     raw_id_fields = ('likes', )
+    inlines = [PropertyownerInline,]
 
-
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('user', 'flat_complaint')
 
-
-class ItemAdmin(admin.ModelAdmin):
-    list_display = (
-        'address', 'price', 'new_building', 'construction_year', 'town'
-    )
-    list_editable = ['new_building']
-    list_filter = ('rooms_number', 'has_balcony', 'new_building')
-
-
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('owners_flats', )
-
-
-admin.site.register(Flat, ItemAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
